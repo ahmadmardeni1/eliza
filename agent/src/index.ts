1a32cb1f40c78fb02da24a1554af507cc68759f7
ahmadmardeni1 <ahmadmardeni0@gmail.com>
2024-12-04 17:59:10 +0700

    character

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -61,6 +61,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import yargs from "yargs";
 
+import { mainCharacter } from '../mainCharacter.ts'
+
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
@@ -203,7 +205,7 @@ export async function loadCharacters(
 
     if (loadedCharacters.length === 0) {
         elizaLogger.info("No characters found, using default character");
-        loadedCharacters.push(defaultCharacter);
+        loadedCharacters.push(mainCharacter);
     }
 
     return loadedCharacters;
@@ -699,7 +701,7 @@ const startAgents = async () => {
 
     let charactersArg = args.characters || args.character;
 
-    let characters = [defaultCharacter];
+    let characters = [mainCharacter];
 
     if (charactersArg) {
         characters = await loadCharacters(charactersArg);
